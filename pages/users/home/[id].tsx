44a1b04692fe3895f250ba51ddae2b612be06b53
varunsharma03 <94775776+varunsharma03@users.noreply.github.com>
2024-09-05 18:08:06 +0530

    Update [id].tsx

--- pages/users/home/[id].tsx
+++ pages/users/home/[id].tsx
@@ -80,7 +80,7 @@ export async function getServerSideProps(context: any) {
   console.log('ID:', id);
 
   try {
-    const response = await fetch(`https://testimonial-g418747ga-varun-sharmas-projects-a3da8c70.vercel.app/api/route/user`, {
+    const response = await fetch(`https://testimonial-g418747ga-varun-sharmas-projects-a3da8c70.vercel.app:3000/api/route/user`, {
       method: 'GET',
       headers: {
         authorization: `Bearer ${id}`,
